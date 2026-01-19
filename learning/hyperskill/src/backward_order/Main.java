1da87176d99903a5026e8a0977d5652f08b048d8
HalinaZaikina <gulyazaikina@gmail.com>
2026-01-19 14:14:23 +0300

    Add backward_order

---
+++ learning/hyperskill/src/backward_order/Main.java
@@ -0,0 +1,9 @@
+package backward_order;
+
+public class Main {
+    public static void main (String[] args) {
+        for (int i = 9; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
